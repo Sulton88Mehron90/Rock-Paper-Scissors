3e74be9896bfb1032b509f0f93a3df7ed6345357
Parvin Sattorova <nanajon66@gmail.com>
2023-04-24 20:45:53 -0600

    fix:displayFighters and reset board functionallity

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Variables:
+var changeGame;
 var game = createGame();
 var userSection = document.getElementById('userSection');
 var userCounter = document.getElementById('userCounter');
@@ -13,19 +14,20 @@ var computerCounter = document.getElementById('computerCounter');
 var fightSection = document.getElementById('fightSection');
 var fighterIcons = document.querySelectorAll('.fighter-icons');
 
-//Event Listeners:
+// Event Listeners:
 classicSection.addEventListener('click', loadClassicGame);
 difficultSection.addEventListener('click', loadDifficultGame);
 changeGameButton.addEventListener('click', changeGame);
 fightSection.addEventListener('click', chooseFighter);
 
-// eventHandlers & functions:
-function createPlayer(user, token, userChoice){
+// functions:
+
+function createPlayer(user, token){
   var player = {
     name: user,
     token: token,
     wins: 0,
-    fighter: userChoice
+    fighter: 0
   };
   return player;
 };
@@ -36,15 +38,16 @@ function takeTurn(userChoice) {
 };
 
 function createGame(gameType){
-  var currentGame = {
+  var game = {
     player: createPlayer('Human', '👩🏾‍💻'),
     computer: createPlayer('Computer', '🖥️'),
     type: gameType,
     fighters: []
   };
+    currentGame = game;
     return currentGame;
 };
-      
+    
 function chooseGame(event) {
   if(event.target.closest('#classic')) {
     game.type = 'classic'
@@ -56,131 +59,134 @@ function chooseGame(event) {
 };
 
 function checkForWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-    return scoreCounter('draw');
-    }else if (computerChoice === '📄' && (userChoice === '✄' || userChoice === '🦎' )) {
-    return scoreCounter('user');
-    }else if (computerChoice === '🪨' && (userChoice === '📄' || userChoice === '👽')) {
-    return scoreCounter('user');
-    }else if (computerChoice === '✄' && (userChoice === '🪨' || userChoice === '👽')) {
-    return scoreCounter('user');
-    }else if (computerChoice === '👽' && (userChoice === '📄' || userChoice === '🦎')) {
-    return scoreCounter('user');
-    }else if (computerChoice === '🦎' && (userChoice === '🪨' || userChoice === '✄')) {
-    return scoreCounter('user');
-    }else {
-    return scoreCounter('computer');
-    };
+  if (userChoice === computerChoice) {
+  return scoreCounter('draw');
+  }else if (computerChoice === '📄' && (userChoice === '✄' || userChoice === '🦎' )) {
+  return scoreCounter('user');
+  }else if (computerChoice === '🪨' && (userChoice === '📄' || userChoice === '👽')) {
+  return scoreCounter('user');
+  }else if (computerChoice === '✄' && (userChoice === '🪨' || userChoice === '👽')) {
+  return scoreCounter('user');
+  }else if (computerChoice === '👽' && (userChoice === '📄' || userChoice === '🦎')) {
+  return scoreCounter('user');
+  }else if (computerChoice === '🦎' && (userChoice === '🪨' || userChoice === '✄')) {
+  return scoreCounter('user');
+  }else {
+  return scoreCounter('computer');
   };
-    
-  function scoreCounter(theWinner) {
-    if (theWinner === 'draw') {
-      duringGameHeader.innerText = 'Its, a Draw! \n \n You both chose the same Fighter, try again!'
-    }else if (theWinner === 'computer') {
-      computer.wins += 1
-      computerCounter.innerText = `Wins: ${computer.wins}`
-      duringGameHeader.innerText =  'Computer won this time!'
-    }else {
-      player.wins += 1
-      userCounter.innerText = `Wins: ${player.wins}`
-      duringGameHeader.innerText =  'You Won!!!'
-    };
+};
+
+function scoreCounter(theWinner) {
+  if (theWinner === 'draw') {
+    duringGameHeader.innerText = 'Its, a Draw! \n \n You both chose the same Fighter, try again!'
+  }else if (theWinner === 'computer') {
+    game.computer.wins += 1
+    computerCounter.innerText = `Wins: ${game.computer.wins}`
+    duringGameHeader.innerText =  'Computer won this time!'
+  }else {
+    game.player.wins += 1
+    userCounter.innerText = `Wins: ${game.player.wins}`
+    duringGameHeader.innerText =  'You Won!!!'
   };
-    
-  function computerFighter() {
-    var computerTurn = getRandomIndex(game.fighters);
-      takeTurn(computerTurn);
-      return fighter;
-  };
-    
-  function getRandomIndex(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  };
+};
   
-  function loadClassicGame(event) {
-    chooseGame(event);
-    
-    hide(classic);
-    hide(difficult);
-    hide(beforeGameHeader);
-    show(changeGameButton);
-    show(duringGameHeader);
-    classicFighters();
-    show(fightSection);
-  };
-    
-  function loadDifficultGame(event) {
-    chooseGame(event); 
-    hide(classic);
-    hide(difficult);
-    show(changeGameButton);
-    hide(beforeGameHeader);
-    show(duringGameHeader);
-    difficultFighters();
-    show(fightSection);
-  };
+function computerFighter() {
+  var computerTurn = getRandomIndex(game.fighters);
+  game.computer.fighter = takeTurn(computerTurn);
+    return fighter;
+};
   
-  function chooseFighter(event) {
-    var fighter = event.target.closest('img').id;
-    takeTurn(fighter);
-    computerFighter();
-    checkForWinner(fighter, fighter);
-    displayFighters(game.player, game.computer) // need to fined a better way on user choice and comp choice
-    fightSection.removeEventListener('click', chooseFighter);
-    setTimeout(resetBoard, 2000);
-    show(duringGameHeader);
+function getRandomIndex(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+function loadClassicGame(event) {
+  chooseGame(event);
+  hide(classic);
+  hide(difficult);
+  hide(beforeGameHeader);
+  show(changeGameButton);
+  show(duringGameHeader);
+  showClassicFighters();
+  show(fightSection);
+};
+
+function loadDifficultGame(event) {
+  chooseGame(event); 
+  hide(classic);
+  hide(difficult);
+  show(changeGameButton);
+  hide(beforeGameHeader);
+  show(duringGameHeader);
+  showDifficultFighters();
+  show(fightSection);
+};
+
+function chooseFighter(event) {
+  if(event.target.className === "fighter-icons"){
+    var fighter = event.target.closest('img').id 
   };
-  
-  function changeGame() {
-    show(classic);
-    show(difficult);
-    show(beforeGameHeader);
-    hide(duringGameHeader);
-    hide(changeGameButton);
-    hide(fightSection);
+  game.player.fighter = takeTurn(fighter);
+  computerFighter()
+  checkForWinner(game.player.fighter, game.computer.fighter);
+  displayFighters(game.player, game.computer);
+  fightSection.removeEventListener('click', chooseFighter);
+  setTimeout(resetBoard, 2000);
+  show(duringGameHeader);
+};
+
+function changeGame() {
+  show(classic);
+  show(difficult);
+  show(beforeGameHeader);
+  hide(duringGameHeader);
+  hide(changeGameButton);
+  hide(fightSection);
+};
+
+function displayFighters(userChoice, computerChoice) {
+  // var fighterIcons = document.querySelectorAll('.fighter-icons');
+  var playerFighter = document.getElementById(userChoice.fighter);
+  var computerFighter = document.getElementById(computerChoice.fighter);
+  fightSection.innerHTML =
+   ` <img class="fighter-icons" id="${userChoice.fighter}"src= "${playerFighter.src}" alt="${playerFighter.alt}">
+    <img class="fighter-icons" id="${computerChoice.fighter}"src= "${computerFighter.src}" alt="${computerFighter.alt}">`
+};
+
+function hide(element) {
+  element.classList.add('hidden');
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
+function showClassicFighters() {
+  fightSection.innerHTML = 
+    `<img class="fighter-icons" id="📄"src= "src/happy-paper.png" alt="Piece of paper">
+     <img class="fighter-icons" id="🪨"src= "src/sang.png" alt="Happy rock">
+     <img class="fighter-icons" id="✄"src= "src/lines-scissors.png" alt="Pair of scissors">`
+};
+
+function showDifficultFighters() {
+  fightSection.innerHTML = 
+    `<img class="fighter-icons" id="📄" src= "src/happy-paper.png" alt="Piece of paper">
+     <img class="fighter-icons" id="🪨"src= "src/sang.png" alt="Happy rock">
+     <img class="fighter-icons" id="✄"src= "src/lines-scissors.png" alt="Pair of scissors">
+     <img class="fighter-icons" id="🦎"src= "src/lizard.webp" alt="Picture of lizard">
+     <img class="fighter-icons" id="👽"src= "src/alien.png" alt="Picture of alien">`
+};
+
+function resetBoard() {
+  // var fighterIcons = document.querySelectorAll('.fighter-icons');
+  for (var i = 0; i < fighterIcons.length; i++) {
+    show(fighterIcons[i]);
   };
-  
-  function displayFighters(userChoice, computerChoice) {
-    var fighterIcons = document.querySelectorAll('.fighter-icons');
-    var playerFighter = document.getElementById(userChoice.fighter);
-    var computerFighter = document.getElementById(computerChoice.fighter);
-  
-    for (var i = 0; i < fighterIcons.length; i++) {
-      hide(fighterIcons[i]);
-    };
-      show(playerFighter);
-      show(computerFighter);
+  duringGameHeader.innerText = 'Choose your fighter!'
+  fightSection.addEventListener('click', chooseFighter);
+  if(game.type === 'classic'){
+    showClassicFighters();
+  }else{
+    showDifficultFighters();
   };
-  
-  function hide(element) {
-    element.classList.add('hidden');
-  };
-  
-  function show(element) {
-    element.classList.remove('hidden');
-  };
-  
-  function classicFighters() {
-    fightSection.innerHTML = 
-      `<img class="fighter-icons" id="📄"src= "src/happy-paper.png" alt="Piece of paper">
-       <img class="fighter-icons" id="🪨"src= "src/sang.png" alt="Happy rock">
-       <img class="fighter-icons" id="✄"src= "src/lines-scissors.png" alt="Pair of scissors">`
-  };
-  
-  function difficultFighters() {
-    fightSection.innerHTML = 
-      `<img class="fighter-icons" id="📄" src= "src/happy-paper.png" alt="Piece of paper">
-       <img class="fighter-icons" id="🪨"src= "src/sang.png" alt="Happy rock">
-       <img class="fighter-icons" id="✄"src= "src/lines-scissors.png" alt="Pair of scissors">
-       <img class="fighter-icons" id="🦎"src= "src/lizard.webp" alt="Picture of lizard">
-       <img class="fighter-icons" id="👽"src= "src/alien.png" alt="Picture of alien">`
-  };
-  
-  function resetBoard() {
-    var fighterIcons = document.querySelectorAll('.fighter-icons');
-    for (var i = 0; i < fighterIcons.length; i++) {
-      show(fighterIcons[i]);
-    };
-    duringGameHeader.innerText = 'Choose your fighter!'
-    fightSection.addEventListener('click', chooseFighter);
-  };
+};
